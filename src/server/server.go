9f19c266e635ce43de306b2456b9bdf17c3da67a
Niclas Åberg <niclas.aberg@gmail.com>
2016-11-10 11:56:11 +0100

    Http server

--- src/server/server.go
+++ src/server/server.go
@@ -1,34 +1,68 @@
 package main
 
-import "net"
+// import "net"
 import "fmt"
+import "log"
+import "net/http"
+import "os"
+import "path/filepath"
 //import "bufio"
 //import "strings" // only needed below for sample processing
-import "./network"
-import (
-    "./game"
-)
+// import "./network"
+// import (
+//     "./game"
+// )
+
+// import (
+//     "fmt"
+//     "code.google.com/p/go.net/websocket"
+//     "net/http"
+// )
+
+// func webHandler(ws *websocket.Conn) {
+//     var in []byte
+//     if err := websocket.Message.Receive(ws, &in); err != nil {
+//         return
+//     }
+//     fmt.Printf("Received: %s\n", string(in))
+//     websocket.Message.Send(ws, in)
+// }
+
 
 func main() {
+    dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+    if err != nil {
+        log.Fatal(err)
+    }
 
-    fmt.Println("Launching server...")
+    fmt.Println("Launching servers...")
 
-    emptyGame := game.CreateGame()
-    go network.RunGame(network.EmptyConnection(), network.EmptyConnection(), emptyGame)
+    http.Handle("/", http.FileServer(http.Dir(dir + "/static")))
+    http.ListenAndServe(":8080", nil)
 
-    // listen on all interfaces
-    ln, _ := net.Listen("tcp", ":8081")
 
-    conn1 := network.NewConnection("Welcome player 1! ", ln)
-    fmt.Println("One player joined")
+    // http.Handle("/", websocket.Handler(webHandler))
+    // err := http.ListenAndServe(":3000", nil)
+    // if err != nil {
+    //     panic("ListenAndServe: " + err.Error())
+    // }
 
-    conn2 := network.NewConnection("Welcome player 2! ", ln)
-    fmt.Println("Two players joined")
+    // emptyGame := game.CreateGame()
+    // go network.RunGame(network.EmptyConnection(), network.EmptyConnection(), emptyGame)
 
-    networkGame := game.CreateGame()
-    go network.RunGame(conn1, conn2, networkGame)
+    // // listen on all interfaces
+    // ln, _ := net.Listen("tcp", ":8081")
 
-    fmt.Print(networkGame)
+    // conn1 := network.NewConnection("Welcome player 1! ", ln)
+    // fmt.Println("One player joined")
+
+    // conn2 := network.NewConnection("Welcome player 2! ", ln)
+    // fmt.Println("Two players joined")
+
+    // networkGame := game.CreateGame()
+    // go network.RunGame(conn1, conn2, networkGame)
+
+    // fmt.Print(networkGame)
 
     // run loop forever (or until ctrl-c)
     i := 0
